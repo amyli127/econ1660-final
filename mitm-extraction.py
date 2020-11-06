7443863735068860cf3e3098138f96118170d5a1
Josh Baum <joshbaum@google.com>
2020-11-06 01:14:34 -0500

    edit query not working

--- mitm-extraction.py
+++ mitm-extraction.py
@@ -1,15 +1,14 @@
 from mitmproxy import http
 import json
 
-count = 0
+# https://api-prod-pvt.snackpass.co/api/v4/gifts?friendsOnly=true&count=50&before=2020-10-30T15:55:10.767Z
 
 def response(flow: http.HTTPFlow) -> None:
-    with open("friend-gifts.txt", "a") as ofile:
+    with open("friend-gifts3.txt", "a") as ofile:
+        # if not flow.request.pretty_url.startswith("https://api-prod-pvt.snackpass.co/api/v4/gifts?friendsOnly=true"):
+        #     return
+
         ofile.write(flow.request.pretty_url)
-
-        # if flow.request.content:
-        #     ofile.write(flow.request.content)
-
         if flow.response.content:
             # resp = str(flow.response.content)
             resp = flow.response.content
@@ -19,3 +18,7 @@ def response(flow: http.HTTPFlow) -> None:
 
         # Add other separators etc. however you want
         ofile.write("-------")
+
+# def request(flow: http.HTTPFlow) -> None:
+#     if flow.request.pretty_url.startswith("https://api-prod-pvt.snackpass.co/api/v4/gifts?friendsOnly=true"):
+#         flow.request.query["count"] = "1"
