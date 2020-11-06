ab9c19798eae6b91ada67699c1573332437741b4
Amy Li <amy_li1@brown.edu>
2020-11-05 23:50:49 -0500

    print extraction

--- mitm-extraction.py
+++ mitm-extraction.py
@@ -1,5 +1,21 @@
 from mitmproxy import http
+import json
+
+count = 0
 
 def response(flow: http.HTTPFlow) -> None:
-    elif flow.request.path.endswith("/brew"):
-        
+    with open("friend-gifts.txt", "a") as ofile:
+        ofile.write(flow.request.pretty_url)
+
+        # if flow.request.content:
+        #     ofile.write(flow.request.content)
+
+        if flow.response.content:
+            # resp = str(flow.response.content)
+            resp = flow.response.content
+            jsonResp = json.loads(resp)
+            respFormatted = json.dumps(jsonResp, indent=4, separators=(". ", " = "))
+            ofile.write(respFormatted)
+
+        # Add other separators etc. however you want
+        ofile.write("-------")
