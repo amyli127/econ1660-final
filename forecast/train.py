71a17c00966479e00d497833a1056689ea6fc3c5
Josh Baum <joshbaum@google.com>
2020-12-09 20:34:51 -0500

    changing thresholds in test

--- forecast/train.py
+++ forecast/train.py
@@ -23,10 +23,32 @@ def train(X_train, y_train, args):
 
 def test(model, X_test, y_test):
 	y_pred = model.predict(X_test)
-	predictions = [round(value) for value in y_pred]
+	# predictions = [round(value) for value in y_pred]
+	predictions = [1 if value > .3 else 0 for value in y_pred]
 
 	accuracy = accuracy_score(y_test, predictions)
 	print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+
+	pred_yes_count = 0
+	pred_yes_actual_yes = 0
+
+	actual_yes = 0
+	actual_yes_pred_yes = 0
+	for i, pred in enumerate(predictions):
+		if pred == 1:
+			pred_yes_count += 1
+			pred_yes_actual_yes += y_test[i]
+		if y_test[i] == 1:
+			actual_yes += 1
+			actual_yes_pred_yes += pred
+	
+	print('predicted yes:', pred_yes_count)
+	print('among the times we predicted there would be an order, this is how many times there was an order: ', pred_yes_actual_yes)
+	print('actually yes:', actual_yes)
+	print('among the times there was an order, this is how many times we predicted there would be an order: ', actual_yes_pred_yes)
+			
+
 
 
 if __name__ == '__main__':
